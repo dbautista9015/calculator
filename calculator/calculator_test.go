c1c1b803f7744811203f4ec0f129f5d2b1072ee9
Daniel Bautista <dbautista9015@gmail.com>
2024-07-30 16:22:17 -0700

    Add Subtract and Multiply functions and test functions

---
+++ calculator/calculator_test.go
@@ -0,0 +1,32 @@
+package calculator_test
+
+import (
+	"github.com/dbautista9015/calculator"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	var want float64 = 4
+	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 100
+	got := calculator.Multiply(50, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
